00ecc1daa322e08e8f44fb094e2cbd77609a952d
bisehshshrestha <bisheshshrestha652@gmail.com>
2025-04-27 10:51:28 +0545

    Animdo animation project created

---
+++ animdo/lib/pages/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //Animated
+  //Animated Container
+  //AnimatedSize
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
