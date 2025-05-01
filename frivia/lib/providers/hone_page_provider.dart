22ba6aae170ca250f59047f312d9d52b2c907875
bisehshshrestha <bisheshshrestha652@gmail.com>
2025-05-01 19:41:34 +0545

    fetch question from the TDB API using the dio and implement the Provider state management

---
+++ frivia/lib/providers/hone_page_provider.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+
+
+class HomePageProvider extends ChangeNotifier {
+  final Dio _dio = Dio();
+  final int maxQuestions = 10;
+  List? questions;
+  int _currentQuestionCount = 0;
+
+  BuildContext context;
+  HomePageProvider({required this.context}){
+    _dio.options.baseUrl = 'https://opentdb.com/api.php';
+    _getQuestionsFromAPI();
+  }
+
+  Future<void> _getQuestionsFromAPI() async{
+    var _response = await _dio.get(
+      '',
+      queryParameters: {
+        'amount':10,
+        'type':'boolean',
+        'difficulty':'easy',
+      },
+    );
+    var _data = jsonDecode(
+      _response.toString(),
+    );
+    questions = _data["results"];
+    notifyListeners();
+  }
+  String getCurrentQuestionText(){
+    return questions![_currentQuestionCount]["question"];
+  }
+}
+
+
