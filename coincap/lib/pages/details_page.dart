49451ac215ef5ca59a6cb7d9f2c9e6b7c75e7977
bisehshshrestha <bisheshshrestha652@gmail.com>
2025-05-01 12:40:28 +0545

    successfully completed the coincap app

---
+++ coincap/lib/pages/details_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class DetailsPage extends StatelessWidget {
+  final Map rates;
+
+  const DetailsPage({super.key, required this.rates});
+
+  @override
+  Widget build(BuildContext context) {
+    List _currencies = rates.keys.toList();
+    List _exchangesRates = rates.values.toList();
+    return Scaffold(
+      body: SafeArea(
+          child: ListView.builder(
+              itemCount: _currencies.length,
+              itemBuilder: (_context, _index){
+                String _currency = _currencies[_index].toString().toUpperCase();
+                String _exchangeRate = _exchangesRates[_index].toString();
+                return ListTile(
+                  title: Text(
+                    "$_currency: $_exchangeRate",
+                        style: const TextStyle(
+                      color: Colors.white,
+                  ),
+                  ),
+                );
+              },
+          ),
+      ),
+    );
+  }
+}
---
+++ coincap/lib/services/http_service.dart
@@ -0,0 +1,28 @@
+import 'package:coincap/models/app_config.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+class HTTPService {
+  final Dio dio = Dio();
+  AppConfig? _appConfig;
+  String? _base_url;
+
+  HTTPService(){
+    _appConfig = GetIt.instance.get<AppConfig>();
+    _base_url = _appConfig!.COIN_API_BASE_URL;
+  }
+
+  Future<Response?> get(String _path) async {
+    try{
+      String _url = "$_base_url$_path";
+      Response _response = await dio.get(_url);
+      return _response;
+    }
+    catch(e){
+      print("HTTPService: Unable to perform a get requests");
+      print(e);
+    }
+  }
+
+
+}
